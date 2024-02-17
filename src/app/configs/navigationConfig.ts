95c53cd62bf39dd1ad833c19967b0a08ed26d2b5
shayan-ziaarabshahi-work <shayan.ziaarabshahi.work@gmail.com>
2024-02-17 15:31:28 +0330

    i18n on fa

--- src/app/configs/navigationConfig.ts
+++ src/app/configs/navigationConfig.ts
@@ -1,12 +1,8 @@
 import i18next from 'i18next';
-import ar from './navigation-i18n/ar';
-import en from './navigation-i18n/en';
-import tr from './navigation-i18n/tr';
+import fa from './navigation-i18n/fa'
 import { NavigationConfig } from '@types-fuse/navigation';
 
-i18next.addResourceBundle('en', 'navigation', en);
-i18next.addResourceBundle('tr', 'navigation', tr);
-i18next.addResourceBundle('ar', 'navigation', ar);
+i18next.addResourceBundle('fa', 'navigation', fa);
 
 const navigationConfig: NavigationConfig[] = [
   {
--- src/app/store/i18nSlice.js
+++ src/app/store/i18nSlice.js
@@ -27,9 +27,7 @@ const i18nSlice = createSlice({
   initialState: {
     language: i18n.options.lng,
     languages: [
-      { id: 'en', title: 'English', flag: 'US' },
-      { id: 'tr', title: 'Turkish', flag: 'TR' },
-      { id: 'ar', title: 'Arabic', flag: 'SA' },
+      { id: 'fa', title: 'persian', flag: 'IR' }
     ],
   },
   reducers: {
