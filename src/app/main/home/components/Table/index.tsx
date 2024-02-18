61c29d4549f710026575bc297adcf5be1aa7a9ab
shayan-ziaarabshahi-work <shayan.ziaarabshahi.work@gmail.com>
2024-02-18 11:34:52 +0330

    example table replaced by more advanced table with actions

--- src/app/main/home/components/Table/index.tsx
+++ src/app/main/home/components/Table/index.tsx
@@ -1,10 +1,22 @@
 import * as React from "react";
 import { prefixer } from "stylis";
 import rtlPlugin from "stylis-plugin-rtl";
-import { DataGrid, GridColDef, faIR } from "@mui/x-data-grid";
+import {
+  DataGrid,
+  GridColDef,
+  faIR,
+  GridActionsCellItem,
+  GridRowId,
+} from "@mui/x-data-grid";
 import createCache from "@emotion/cache";
 import { CacheProvider } from "@emotion/react";
 import { createTheme, ThemeProvider, useTheme } from "@mui/material/styles";
+import {
+  Delete as DeleteIcon,
+  Security as SecurityIcon,
+  FileCopy as FileCopyIcon,
+} from "@mui/icons-material";
+import { Box } from "@mui/system";
 
 // Create rtl cache
 const cacheRtl = createCache({
@@ -12,43 +24,35 @@ const cacheRtl = createCache({
   stylisPlugins: [prefixer, rtlPlugin],
 });
 
-const columns: GridColDef[] = [
+// rows
+const initialRows = [
   {
-    field: "id",
-    headerName: "شناسه",
-    width: 150,
+    id: 1,
+    name: "علی احمدی",
+    age: 25,
+    citizenship: true,
   },
   {
-    field: "name",
-    headerName: "نام",
-    width: 150,
+    id: 2,
+    name: "مصطفی محمدی",
+    age: 36,
+    citizenship: false,
   },
   {
-    field: "age",
-    headerName: "سن",
-    valueGetter: (params) => `${params.value} سن`,
-    width: 150,
-  },
-  {
-    field: "occupation",
-    headerName: "شغل",
-    width: 150,
-  },
-  {
-    field: "gender",
-    headerName: "جنس",
-    width: 150,
+    id: 3,
+    name: "کاظم سعیدی",
+    age: 19,
+    citizenship: false,
   },
 ];
 
-const rows = [
-  { id: 1, name: "شایان", age: 28, occupation: "برنامه نویس", gender: "مرد" },
-];
+type Row = typeof initialRows[number];
 
 export default function DataGridRTL() {
-  // Inherit the theme from the docs site (dark/light mode)
-  const existingTheme = useTheme();
+  const [rows, setRows] = React.useState<Row[]>(initialRows);
 
+  /* theme */
+  const existingTheme = useTheme();
   const theme = React.useMemo(
     () =>
       createTheme({}, faIR, existingTheme, {
@@ -56,15 +60,84 @@ export default function DataGridRTL() {
       }),
     [existingTheme]
   );
+
+  /* actions functions */
+  const deleteUser = React.useCallback(
+    (id: GridRowId) => () => {
+      setTimeout(() => {
+        setRows((prevRows) => prevRows.filter((row) => row.id !== id));
+      });
+    },
+    []
+  );
+
+  const toggleCitizenship = React.useCallback(
+    (id: GridRowId) => () => {
+      setRows((prevRows) =>
+        prevRows.map((row) =>
+          row.id === id ? { ...row, citizenship: !row.citizenship } : row
+        )
+      );
+    },
+    []
+  );
+
+  const duplicateUser = React.useCallback(
+    (id: GridRowId) => () => {
+      setRows((prevRows) => {
+        const rowToDuplicate = prevRows.find((row) => row.id === id)!;
+        return [...prevRows, { ...rowToDuplicate, id: Date.now() }];
+      });
+    },
+    []
+  );
+
+  /* columns */
+  const columns = React.useMemo<GridColDef<Row>[]>(
+    () => [
+      { field: "name", headerName: "نام", type: "string" },
+      { field: "age", headerName: "سن", type: "number" },
+      {
+        field: "citizenship",
+        headerName: "تابعیت",
+        type: "boolean",
+        width: 120,
+      },
+      {
+        field: "actions",
+        headerName: "عملیات",
+        type: "actions",
+        width: 80,
+        getActions: (params: { id: GridRowId }) => [
+          <GridActionsCellItem
+            icon={<DeleteIcon />}
+            label="Delete"
+            onClick={deleteUser(params.id)}
+          />,
+          <GridActionsCellItem
+            icon={<SecurityIcon />}
+            label="تغییر تابعیت"
+            onClick={toggleCitizenship(params.id)}
+            showInMenu
+          />,
+          <GridActionsCellItem
+            icon={<FileCopyIcon />}
+            label="رونویسی کاربر"
+            onClick={duplicateUser(params.id)}
+            showInMenu
+          />,
+        ],
+      },
+    ],
+    [deleteUser, toggleCitizenship, duplicateUser]
+  );
+
   return (
     <CacheProvider value={cacheRtl}>
       <ThemeProvider theme={theme}>
-        <div dir="rtl" style={{ height: 400, width: "100%" }}>
-          <DataGrid
-            rows={rows}
-            columns={columns}
-          />
-        </div>
+        <Box dir="rtl" style={{ height: 400, width: "100%" }} className="">
+          <DataGrid rows={rows} columns={columns} />
+        </Box>
       </ThemeProvider>
     </CacheProvider>
   );
